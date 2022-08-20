1c42e5eddf78844b087a040050458b0332f31bf1
Adriel <Adriel@192.168.5.101>
2022-08-20 20:09:57 -0300

    Fazendo um commit

--- estudo-java-fiap/src/com/estudofiap/javafundments/OlaFiap.java
+++ estudo-java-fiap/src/com/estudofiap/javafundments/OlaFiap.java
@@ -1,7 +1,7 @@
 package com.estudofiap.javafundments;
 
 public class OlaFiap {
-
+	// value
 	public static void main(String[] args) {
 		System.out.println("FIAP - voltando a estudar o curso do Edu-m-Brasil");
 
